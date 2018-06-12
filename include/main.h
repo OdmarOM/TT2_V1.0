e5116191ae2e183160bc4397a23e8f9be3466a9d
Ohtokani <ohtopinzon@gmail.com>
2018-06-12 01:19:06 -0500

    firs commit

---
+++ include/main.h
@@ -0,0 +1,12 @@
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#include "Robot.h"
+#include "OsObjects.h"
+#include "cmsis_os.h"
+#include "utilities.h"
+#include "HardwareInit.h"
+
+SerialUSART2 usart(9600);
+
+#endif //__DELAY_H
